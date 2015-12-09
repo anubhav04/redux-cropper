83da5cd0f9b2b075a3145cf6f0a653af633642e2
Sergey Lapin <lapanoid@gmail.com>
2015-12-09 13:15:34 +0300

    chore(package.json): add custom commitizen

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,4 +1,4 @@
-export {default as CropBoxSelector} from './CropBox'
-export {default as ImageSelector} from './Image'
-export {containerSelector} from './share'
-export {optionsSelector} from './share'
+export { default as CropBoxSelector } from './CropBox'
+export { default as ImageSelector } from './Image'
+export { containerSelector } from './share'
+export { optionsSelector } from './share'
